ab544941a5d63afc0c65189b1552196014ab5046
Victor Chen <victor.chen@hoc-trade.com>
2025-06-25 13:59:00 +0800

    refactor: deprecate useContractRead

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,11 +8,11 @@ import { hardhat } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const config = createConfig({
+  chains: [hardhat],
   connectors: [injected()],
   transports: {
-    [hardhat.id]: http(),
+    [hardhat.id]: http("http://127.0.0.1:8545"),
   },
-  chains: [hardhat],
 });
 
 const container = document.getElementById("root");
