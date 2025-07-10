4f4a95be0aa18b996f7a20ab243a85b166d7f561
Victor Chen <victor.chen@hoc-trade.com>
2025-07-10 16:29:56 +0800

    feature: implement create immigrate

--- backend/src/routers/immigrates-router.ts
+++ backend/src/routers/immigrates-router.ts
@@ -3,12 +3,22 @@ import { Router } from "express";
 
 const router = Router();
 
+const immigrates = [
+  { name: "Canada" },
+  { name: "Germany" },
+  { name: "Australia" },
+];
+
 router.get("/", (req, res) => {
-  res.json([
-    { name: "Canada" },
-    { name: "Germany" },
-    { name: "Australia" }
-  ]);
+  res.json(immigrates);
+});
+
+router.post("/", (req, res) => {
+  const { name } = req.body;
+  if (!name) return res.status(400).json({ error: "Name is required" });
+
+  immigrates.push({ name });
+  res.status(201).json({ success: true });
 });
 
 export default router;
