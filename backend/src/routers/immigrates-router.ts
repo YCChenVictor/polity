e818fefc3e2edc43f86710413734884de02891b0
Victor Chen <victor.chen@hoc-trade.com>
2025-07-11 17:00:02 +0800

    feature: implement create immigration application

--- backend/src/routers/immigrates-router.ts
+++ backend/src/routers/immigrates-router.ts
@@ -4,21 +4,35 @@ import { Router } from "express";
 const router = Router();
 
 const immigrates = [
-  { name: "Canada" },
-  { name: "Germany" },
-  { name: "Australia" },
+  { name: "Canada", wallet_address: "" },
+  { name: "Germany",  wallet_address: "" },
+  { name: "Australia",  wallet_address: "" },
 ];
+
+// Directly create applications here
+// There will be a mechanism to create the applications
+let applications = [
+  {name: "haha", wallet_address: "test", origin: "", reason: "", assets: ""},
+  {name: "haha", wallet_address: "test1", origin: "", reason: "", assets: ""}
+]
 
 router.get("/", (req, res) => {
   res.json(immigrates);
 });
 
-router.post("/", (req, res) => {
-  const { name } = req.body;
-  if (!name) return res.status(400).json({ error: "Name is required" });
+router.get("/applications", (req, res) => {
+  res.json(applications);
+});
 
-  immigrates.push({ name });
-  res.status(201).json({ success: true });
+router.post("/", (req, res) => {
+  const { name, wallet_address } = req.body;
+  if (!name) return res.status(400).json({ error: "Name is required" });
+  if (!wallet_address) return res.status(400).json({ error: "Wallet is required" });
+
+  immigrates.push({name, wallet_address })
+  applications = applications.filter(app => app.wallet_address !== wallet_address);
+
+  res.status(201).json(applications);
 });
 
 export default router;
