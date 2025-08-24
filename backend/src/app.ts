ecb9b561b83824b7f760b54a3e3010e7f9e2f127
Victor Chen <victor.chen@hoc-trade.com>
2025-08-24 22:31:51 +0800

    feature: implement-SIWE

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,31 @@
 import express from "express";
 import cors from "cors";
+
+import "./env";
+
 import immigratesRouter from "./routers/immigrates-router";
 import contributionsRouter from "./routers/contribution-router";
 import legislatorRouter from "./routers/legislator-router";
 import bullRouter from "./routers/bill-router";
-import eventsRouter from "./routers/events-router";
+// import eventsRouter from "./routers/events-router";
+import authRouter from "./routers/auth-router";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_ENDPOINT,
+    methods: ["POST"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
+
 app.use("/immigrates", immigratesRouter);
 app.use("/contributions", contributionsRouter);
 app.use("/legislators", legislatorRouter);
 app.use("/bills", bullRouter);
-app.use("/events", eventsRouter);
+// app.use("/events", eventsRouter);
+app.use("/auth", authRouter)
 
 export default app;
